f2958b48becc89ac0a6c779362c4efd65d042654
CharwekarAkshay <charwekar12@gmail.com>
2020-08-22 22:25:27 +0530

    Created models

---
+++ lib/models/user_model.dart
@@ -0,0 +1,11 @@
+import "package:meta/meta.dart";
+
+class User {
+  final String username;
+  final String imageUrl;
+
+  User({
+    @required this.username,
+    @required this.imageUrl,
+  });
+}
