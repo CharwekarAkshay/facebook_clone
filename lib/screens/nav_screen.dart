c0bd462f53b5e0a1b51608718c8370ba3e325b3d
CharwekarAkshay <charwekar12@gmail.com>
2020-08-24 14:33:52 +0530

    Done with custom app bar

--- lib/screens/nav_screen.dart
+++ lib/screens/nav_screen.dart
@@ -1,3 +1,4 @@
+import 'package:facebook_clone/data/data.dart';
 import 'package:facebook_clone/screens/screens.dart';
 import 'package:facebook_clone/widgets/widgets.dart';
 import 'package:flutter/material.dart';
@@ -29,22 +30,40 @@ class _NavScreenState extends State<NavScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+
     return DefaultTabController(
       length: _icons.length,
       child: Scaffold(
+        appBar: Responsive.isDesktop(context)
+            ? PreferredSize(
+                preferredSize: Size(
+                  screenSize.width,
+                  100,
+                ),
+                child: CustomAppBar(
+                  currentUser: currentUser,
+                  icons: _icons,
+                  selectedIndex: _selectedIndex,
+                  onTap: (index) => setState(() => _selectedIndex = index),
+                ),
+              )
+            : null,
         body: IndexedStack(
           index: _selectedIndex,
           children: _screens,
         ),
-        bottomNavigationBar: Container(
-          padding: const EdgeInsets.only(bottom: 12.0),
-          color: Colors.white,
-          child: CustomTabBar(
-            icons: _icons,
-            selectedIndex: _selectedIndex,
-            onTap: (index) => setState(() => _selectedIndex = index),
-          ),
-        ),
+        bottomNavigationBar: !Responsive.isDesktop(context)
+            ? Container(
+                padding: const EdgeInsets.only(bottom: 12.0),
+                color: Colors.white,
+                child: CustomTabBar(
+                  icons: _icons,
+                  selectedIndex: _selectedIndex,
+                  onTap: (index) => setState(() => _selectedIndex = index),
+                ),
+              )
+            : const SizedBox.shrink(),
       ),
     );
   }
